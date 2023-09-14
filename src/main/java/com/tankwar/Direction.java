2eab285a9aa51145c1202344de2bc4a70109786e
leo tseng <leo.tseng@varmeego.com>
2023-09-14 15:31:21 +0800

    tank move module

---
+++ src/main/java/com/tankwar/Direction.java
@@ -0,0 +1,13 @@
+package com.tankwar;
+
+public enum Direction {
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT,
+}
--- src/main/java/com/tankwar/GameClient.java
+++ src/main/java/com/tankwar/GameClient.java
@@ -2,18 +2,22 @@ package com.tankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
+    private Tank playerTank;
+
     private GameClient() {
+        this.playerTank = new Tank(600, 100, Direction.DOWN);
+
         this.setPreferredSize(new Dimension(800, 600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(), 400, 100, null);
+        this.playerTank.draw(g);
     }
 
     public static void main(String[] args) {
@@ -24,7 +28,27 @@ public class GameClient extends JComponent {
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playerTank.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyRelease(e);
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        while (true) {
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
